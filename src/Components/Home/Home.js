9660c53e3fd1f828e0119dfb594cf0a98f83884d
Wasif007 <wasifateeq007@hotmail.com>
2023-08-13 22:15:37 +0500

    Styling to make perfect dup

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import "./home.scss"
+const Card=({img})=>{
+    return (
+        <img className="card" src={img} alt='imageUsed'></img>
+    )
+}
+
+const Row=({title})=>{
+    
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+            <Card img={"https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg"}/>
+
+            </div>
+        </div>
+    )
+}
+
+
+
+
 const Home = () => {
   return (
-    <div>
-      Home Page
+   <section className='home'>
+    <div className="banner">
+
     </div>
+    <Row title={"Popular on Netflix"}/>
+   </section>
+   
   )
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./navbar.scss"
 import logoimg from "../../logo.png"; 
+import { Link } from 'react-router-dom';
+import {GoSearch} from 'react-icons/go';
 const Navbar = () => {
     
   return (
     <nav className='header'>
-     <img src={logoimg} alt='logoimg'></img> 
+     <img src={logoimg} alt='logoimg'></img>
+    <div>
+        <Link to="/tvshows">TV Shows</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/recentlyadded">Recently Added</Link>
+        <Link to="/mylist">My List</Link>
+    </div> 
+    <GoSearch/>
     </nav>
   )
 }
